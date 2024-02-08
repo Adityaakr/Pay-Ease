0280dde815d84d6dc6c1036e04abd35542d5ce37
Aditya Kumar <08quant@gmail.com>
2024-02-08 17:11:37 +0530

    done

--- frontend/src/components/AppBar.jsx
+++ frontend/src/components/AppBar.jsx
@@ -15,8 +15,8 @@ export function AppBar(props) {
         <div class="flex items-center md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse m-5">
       <button type="button" class="flex text-sm rounded-md md:me-0 focus:ring-4 focus:ring-gray-300 " id="user-menu-button" 
       aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
-        <p className='p-3 text-black font-semibold text-sm shadow-md mb-3'>Name</p>
-        <img class="w-8 h-8 rounded-full mt-2 mr-2 mb-1 ml-2" src="https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg" alt="user photo"/>
+        <p className='p-3 text-black font-semibold text-sm shadow-md -mt-1'>Name</p>
+        <img class="w-9 h-9 rounded-full mt-1 mr-2 mb-1 ml-2" src="https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg" alt="user photo"/>
       </button>
       </div>
         </ul>
