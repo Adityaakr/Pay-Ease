4e56e009c2dbfc8bc69549f7a5f21173a205ab2b
Aditya Kumar <08quant@gmail.com>
2024-02-08 17:08:21 +0530

    yes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,6 +5,7 @@ import Signup from './Pages/Signup.jsx'
 import Signin from './Pages/Signin.jsx'
 import App from './App.jsx'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { AppBar } from './components/AppBar.jsx'
 
 
 const router = createBrowserRouter([
@@ -25,6 +26,7 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-   <RouterProvider router={router}/>
+   {/* <RouterProvider router={router}/> */}
+   <AppBar/>
   </React.StrictMode>,
 )
