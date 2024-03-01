470b5e500ed8379fdaca02d62dd9922ebbcd927e
Aditya Kumar <08quant@gmail.com>
2024-03-02 03:19:22 +0530

    commit-5

--- frontend/src/Pages/SendMoney.jsx
+++ frontend/src/Pages/SendMoney.jsx
@@ -2,9 +2,8 @@ import { useNavigate, useSearchParams } from "react-router-dom";
 import axios from "axios";
 import { useState } from "react";
 
-import { ToastContainer, toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 export const SendMoney = () => {
   const [searchParams] = useSearchParams();
   const id = searchParams.get("id");
@@ -44,7 +43,6 @@ export const SendMoney = () => {
 
   return (
     <>
-      <ToastContainer />
       <div
         className="flex justify-center h-screen "
         style={{ backgroundImage: "url('/patterns.jpg')" }}
@@ -99,6 +97,7 @@ export const SendMoney = () => {
                   Initiate Transfer
                 </button>
               </div>
+              <ToastContainer />
             </div>
           </div>
         </div>
