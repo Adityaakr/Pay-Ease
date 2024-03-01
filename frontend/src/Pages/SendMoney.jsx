1afa890c0212fc4cac50dcb040e35697ff177176
Aditya Kumar <08quant@gmail.com>
2024-03-01 15:27:14 +0530

    last

--- frontend/src/Pages/SendMoney.jsx
+++ frontend/src/Pages/SendMoney.jsx
@@ -2,6 +2,9 @@ import { useNavigate, useSearchParams } from "react-router-dom";
 import axios from "axios";
 import { useState } from "react";
 
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 export const SendMoney = () => {
   const [searchParams] = useSearchParams();
   const id = searchParams.get("id");
@@ -11,7 +14,7 @@ export const SendMoney = () => {
 
   const handleTransfer = () => {
     if (!amount) {
-      alert("Please enter the amount.");
+      toast.error("Please Enter the Amount", {});
       return;
     }
 
@@ -30,71 +33,76 @@ export const SendMoney = () => {
       )
       .then((response) => {
         if (response.status === 200) {
-          alert("Transaction Successful");
-          navigate("/dashboard")
-        } else {
-          alert("Transaction Failed");
-        }
+          toast.success("Transaction Succesfull", {})
+        } setTimeout(() => {
+          return navigate("/dashboard")
+        },3000)
+      }).catch(() => {
+        toast.error("Insufficient Balance", {})
       })
-      .catch((error) => {
-        alert("Transaction Failed");
-      });
   };
 
   return (
-    <div
-      className="flex justify-center h-screen "
-      style={{backgroundImage: "url('/public/patterns.jpg')"}}
-    >
-      <div className="h-full flex flex-col justify-center">
-        <div className="border h-min text-card-foreground max-w-md p-4 
-        space-y-8 w-96 bg-white shadow-lg rounded-lg">
-          <div className="flex flex-col space-y-1.5 p-6">
-            <h2 className="text-3xl font-bold text-center">Send Money</h2>
-          </div>
-          <div className="p-6">
-            <div className="flex items-center space-x-4">
-              <div className="w-12 h-12 rounded-full bg-blue-500
-               flex items-center justify-center">
-                <span className="text-2xl text-white">
-                  {name[0].toUpperCase()}
-                </span>
-              </div>
-              <h3 className="text-2xl font-semibold">{name}</h3>
+    <>
+      <ToastContainer />
+      <div
+        className="flex justify-center h-screen "
+        style={{ backgroundImage: "url('/public/patterns.jpg')" }}
+      >
+        <div className="h-full flex flex-col justify-center">
+          <div
+            className="border h-min text-card-foreground max-w-md p-4 
+        space-y-8 w-96 bg-white shadow-lg rounded-lg"
+          >
+            <div className="flex flex-col space-y-1.5 p-6">
+              <h2 className="text-3xl font-bold text-center">Send Money</h2>
             </div>
-            <div className="space-y-4">
-              <div className="space-y-2">
-                <label
-                  className="text-sm font-medium leading-none 
+            <div className="p-6">
+              <div className="flex items-center space-x-4">
+                <div
+                  className="w-12 h-12 rounded-full bg-blue-500
+               flex items-center justify-center"
+                >
+                  <span className="text-2xl text-white">
+                    {name[0].toUpperCase()}
+                  </span>
+                </div>
+                <h3 className="text-2xl font-semibold">{name}</h3>
+              </div>
+              <div className="space-y-4">
+                <div className="space-y-2">
+                  <label
+                    className="text-sm font-medium leading-none 
                   peer-disabled:cursor-not-allowed 
                   peer-disabled:opacity-70"
-                  htmlFor="amount"
-                >
-                  Amount (in Rs)
-                </label>
-                <input
-                  onChange={(e) => {
-                    setAmount(e.target.value);
-                  }}
-                  type="number"
-                  className="flex h-10 w-full rounded-md border 
+                    htmlFor="amount"
+                  >
+                    Amount (in Rs)
+                  </label>
+                  <input
+                    onChange={(e) => {
+                      setAmount(e.target.value);
+                    }}
+                    type="number"
+                    className="flex h-10 w-full rounded-md border 
                   border-input bg-background px-3 py-2 text-sm"
-                  id="amount"
-                  placeholder="Enter amount"
-                />
-              </div>
-              <button
-                onClick={handleTransfer}
-                className="justify-center rounded-md text-sm font-medium 
+                    id="amount"
+                    placeholder="Enter amount"
+                  />
+                </div>
+                <button
+                  onClick={handleTransfer}
+                  className="justify-center rounded-md text-sm font-medium 
                 ring-offset-background transition-colors h-10 px-4 py-2 w-full
                  bg-blue-600 text-white"
-              >
-                Initiate Transfer
-              </button>
+                >
+                  Initiate Transfer
+                </button>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
